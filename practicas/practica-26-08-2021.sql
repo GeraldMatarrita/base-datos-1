b691f67756567d7e0c74ca0b65891abb2cb69cf9
Gerald Matarrita <68918784+GeraldMatarrita@users.noreply.github.com>
2021-08-26 19:27:20 -0600

    Update practica-26-08-2021.sql

--- practicas/practica-26-08-2021.sql
+++ practicas/practica-26-08-2021.sql
@@ -8,11 +8,11 @@ select * from people;
 describe people;
 show create table people;
 
+
+-- Tabla planetas
 create table if not exists planet(
 id int primary key auto_increment, name varchar(50), orbital_period int, rotation_period int,
 diameter int, climate varchar(20), gravity varchar(20), terrain varchar(20), surface_water int, population long);
-
-drop table planet;
 
 -- insertar pesonas
 insert into people(id, name, mass, hair_color, eye_color, gender, planet_id) values (1, 'Luke Skywalker', 77, 'blond', 'blue', 'male',1);
@@ -28,6 +28,7 @@ values ( 'Corelia', 304, 23, 10465, 'arid', '1 standard', 'desert', 1, 200000);
 
 drop table people;
 
+-- Tabla personas
 create table people (
     id int auto_increment,
     name varchar(50),
